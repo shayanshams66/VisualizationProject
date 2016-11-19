3532cc925a9520bae421471ff9e113d13c7ca974
Grant Williams <seellsu@gmail.com>
2016-11-19 16:10:00 -0600

    Make a simple triangle in OpenGL
    This is the basic template we'll be using. The interface will be done
    through HTML forms for ease of programming (except arrow keys for
    rotation).

--- code/init.js
+++ code/init.js
@@ -1,6 +1,20 @@
 var gl; //gl context handle object 
 var program; //shader program 
 
+//Force the canvas to be the right size. Adapted from: http://webglfundamentals.org/webgl/lessons/webgl-resizing-the-canvas.html
+function forceCanvasSize(canvas) {
+	var displayWidth  = canvas.clientWidth,
+		displayHeight = canvas.clientHeight
+ 
+  // Check if the canvas is not the same size.
+	if (canvas.width  != displayWidth ||
+		canvas.height != displayHeight) {
+			
+		canvas.width  = displayWidth;
+		canvas.height = displayHeight;
+	}
+}
+
 function init() {
 	var canvas = document.getElementById("glcanvas")
 	
@@ -40,11 +54,44 @@ function init() {
 	var f_shader = createShader( gl, gl.FRAGMENT_SHADER, f_shader_src )
 	var program = createProgram( gl, v_shader, f_shader )
 	
-	gl.enable( gl.DEPTH_TEST )
-	gl.depthFunc( gl.LEQUAL )
-	gl.clearColor( 0, 0, 0, 1 )
-	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT )
+	var positionLoc = gl.getAttribLocation(program, "a_position")
+	var positionBuf = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuf)
+	
+	//position attribute buf parameters
+	var size = 3,
+		type = gl.FLOAT,
+		normalize = false,
+		stride = 0,
+		offset = 0
+	
+	gl.vertexAttribPointer( positionBuf, size, type, normalize, stride, offset)
+
+	// three 2d points
+	var positions = [
+	  -0.5, -0.5, 0,
+	  0.5, -0.5, 0,
+	  0, 0.5, 0
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+		
+	//gl.enable( gl.DEPTH_TEST )
+	//gl.depthFunc( gl.LEQUAL )
+	gl.clearColor( 0, 0, 1, 1 )
+	
+	gl.useProgram(program)
+	gl.enableVertexAttribArray(positionLoc)
+	
+	renderScene()
 }
 
-
+function renderScene() {
+	forceCanvasSize(gl.canvas)
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT )
+	
+	var offset = 0, count = 3
+	
+	gl.drawArrays( gl.TRIANGLES, offset, count ); 
+}
 
